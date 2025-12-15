b40db96fbf9965faca3b162f0ceb87cadf6d07f9
momo <momomomochinese@gmail.com>
2025-12-15 21:45:00 +0800

    支持了添加依赖
    Commit at 2025-12-15 21:45:00

--- Runtime/UPMData.cs
+++ Runtime/UPMData.cs
@@ -9,11 +9,26 @@ namespace Momos.UnityGitPack.Common {
             public string email;
         }
 
+        [Serializable]
+        public class UPMDependency {
+            /// <summary> 包名 如com.unity.textmeshpro </summary>
+            public string name;
+
+            /// <summary> 版本 / 地址： 
+            /// 1.0.0 #如果是官方插件
+            /// https://github.com/user/repo.git#v1.2.0 #github上的插件
+            /// file:../SomePackage #本地文件
+            /// </summary>
+            public string reference;
+        }
+
         public string name;
         public string version;
         public string displayName;
         public string description;
         public string unity;
         public List<AuthorData> author = new List<AuthorData>() { new AuthorData() };
+
+        public List<UPMDependency> dependencies = new List<UPMDependency>();
     }
 }
